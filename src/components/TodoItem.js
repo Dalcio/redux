5047eb915035c29898545411bbee93b43b510ce6
Dálcio Macuete Garcia <dalciomacuetegarcia@gmail.com>
2022-01-04 22:31:59 +0100

    feat: finish todo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+
+import { deleteTodo, toggleComplete } from "../redux/todoSlice";
 
 const TodoItem = ({ id, title, completed }) => {
-	return (
-		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
-			<div className='d-flex justify-content-between'>
-				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}></input>
-					{title}
-				</span>
-				<button className='btn btn-danger'>Delete</button>
-			</div>
-		</li>
-	);
+  const dispatch = useDispatch();
+
+  const handleCheckboxClick = () => {
+    dispatch(toggleComplete({ id, completed: !completed }));
+  };
+
+  const handleDeleteClick = () => {
+    dispatch(deleteTodo({ id }));
+  };
+
+  return (
+    <li className={`list-group-item ${completed && "list-group-item-success"}`}>
+      <div className="d-flex justify-content-between">
+        <span className="d-flex align-items-center">
+          <input
+            type="checkbox"
+            className="mr-3"
+            checked={completed}
+            // onClick={handleCheckboxClick}
+            onChange={handleCheckboxClick}
+          ></input>
+          {title}
+        </span>
+        <button className="btn btn-danger" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
+import { Provider } from "react-redux";
 
-import App from './App';
+import "./index.css";
+
+import store from "./redux/store";
+
+import App from "./App";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
