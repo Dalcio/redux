81fb04d7408eb67c0db926251eb21115b495df16
Dálcio Macuete Garcia <dalciomacuetegarcia@gmail.com>
2022-01-05 10:21:28 +0100

    feat: redux with dave ceddia start

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Provider } from "react-redux";
-
 import "./index.css";
-
-import store from "./redux/store";
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
